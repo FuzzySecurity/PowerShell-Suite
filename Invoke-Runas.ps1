a06671c735a75df89f63264c83320dbabc4890e9
FuzzySecurity <ru.boonen@gmail.com>
2016-03-13 19:53:20 +0000

    Added files via upload

--- Invoke-Runas.ps1
+++ Invoke-Runas.ps1
@@ -3,34 +3,34 @@ function Invoke-Runas {
 <#
 .SYNOPSIS
 
-    Overview:
-    
-    Functionally equivalent to Windows "runas.exe", using Advapi32::CreateProcessWithLogonW. Advapi32::GetTokenInformation is not necessary here but was added for reference.
-    
-    Parameters:
+	Overview:
+	
+	Functionally equivalent to Windows "runas.exe", using Advapi32::CreateProcessWithLogonW. Advapi32::GetTokenInformation is not necessary here but was added for reference.
+	
+	Parameters:
 
-     -User              Specifiy username.
-     
-     -Password          Specify password.
-     
-     -Domain            Specify domain. Defaults to localhost if not specified.
-     
-     -LogonType         dwLogonFlags:
-                          0x00000001 --> LOGON_WITH_PROFILE
-                                           Log on, then load the user profile in the HKEY_USERS registry
-                                           key. The function returns after the profile is loaded.
-                                           
-                          0x00000002 --> LOGON_NETCREDENTIALS_ONLY (= /netonly)
-                                           Log on, but use the specified credentials on the network only.
-                                           The new process uses the same token as the caller, but the
-                                           system creates a new logon session within LSA, and the process
-                                           uses the specified credentials as the default credentials.
-     
-     -Binary            Full path of the module to be executed.
-                       
-     -Args              Arguments to pass to the module, e.g. "/c calc.exe". Defaults
-                        to $null if not specified.
-                       
+	 -User			  Specifiy username.
+	 
+	 -Password		  Specify password.
+	 
+	 -Domain			Specify domain. Defaults to localhost if not specified.
+	 
+	 -LogonType		 dwLogonFlags:
+						  0x00000001 --> LOGON_WITH_PROFILE
+										   Log on, then load the user profile in the HKEY_USERS registry
+										   key. The function returns after the profile is loaded.
+										   
+						  0x00000002 --> LOGON_NETCREDENTIALS_ONLY (= /netonly)
+										   Log on, but use the specified credentials on the network only.
+										   The new process uses the same token as the caller, but the
+										   system creates a new logon session within LSA, and the process
+										   uses the specified credentials as the default credentials.
+	 
+	 -Binary			Full path of the module to be executed.
+					   
+	 -Args			  Arguments to pass to the module, e.g. "/c calc.exe". Defaults
+						to $null if not specified.
+					   
 
 .DESCRIPTION
 	Author: Ruben Boonen (@FuzzySec)
@@ -47,132 +47,132 @@ function Invoke-Runas {
 #>
 
 	param (
-        [Parameter(Mandatory = $True)]
+		[Parameter(Mandatory = $True)]
 		[string]$User,
-        [Parameter(Mandatory = $True)]
+		[Parameter(Mandatory = $True)]
 		[string]$Password,
-        [Parameter(Mandatory = $False)]
+		[Parameter(Mandatory = $False)]
 		[string]$Domain=".",
-        [Parameter(Mandatory = $True)]
+		[Parameter(Mandatory = $True)]
 		[string]$Binary,
-        [Parameter(Mandatory = $False)]
+		[Parameter(Mandatory = $False)]
 		[string]$Args=$null,
-        [Parameter(Mandatory = $True)]
+		[Parameter(Mandatory = $True)]
 		[string]$LogonType
 	)  
 
-    Add-Type -TypeDefinition @"
-    using System;
-    using System.Diagnostics;
-    using System.Runtime.InteropServices;
-    using System.Security.Principal;
-    
-    [StructLayout(LayoutKind.Sequential)]
-    public struct PROCESS_INFORMATION
-    {
-        public IntPtr hProcess; public IntPtr hThread; public uint dwProcessId; public uint dwThreadId;
-    }
-    
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-    public struct STARTUPINFO
-    {
-        public uint cb; public string lpReserved; public string lpDesktop; public string lpTitle;
-        public uint dwX; public uint dwY; public uint dwXSize; public uint dwYSize; public uint dwXCountChars;
-        public uint dwYCountChars; public uint dwFillAttribute; public uint dwFlags; public short wShowWindow;
-        public short cbReserved2; public IntPtr lpReserved2; public IntPtr hStdInput; public IntPtr hStdOutput;
-        public IntPtr hStdError;
-    }
-    
-    [StructLayout(LayoutKind.Sequential, Size=8)]
-    struct LARGE_INTEGER
-    {
-    public uint LowPart; public int HighPart;
-    }
-    
-    [StructLayout(LayoutKind.Sequential, Size=8)]
-    public struct LUID {
-    public uint LowPart; public int HighPart;
-    }
-    
-    [StructLayout(LayoutKind.Sequential)]
-    public struct TOKEN_STATISTICS
-    {
-        LUID _tokenId; LUID _authenticationId; LARGE_INTEGER _expirationTime; public uint _tokenType;
-        public uint _impersonationLevel; public uint _dynamicCharged; public uint _dynamicAvailable;
-        public uint _groupCount; public uint _privilegeCount; LUID _modifiedId;
-    }
-    
-    public static class Advapi32
-    {
-        [DllImport("advapi32.dll", SetLastError=true, CharSet=CharSet.Unicode)]
-        public static extern bool CreateProcessWithLogonW(
-            String userName,
-            String domain,
-            String password,
-            int logonFlags,
-            String applicationName,
-            String commandLine,
-            int creationFlags,
-            int environment,
-            String currentDirectory,
-            ref  STARTUPINFO startupInfo,
-            out PROCESS_INFORMATION processInformation);
-        
-        [DllImport("advapi32.dll", SetLastError=true, CharSet=CharSet.Unicode)]
-        public static extern bool OpenProcessToken(
-            IntPtr ProcessHandle, 
-            uint DesiredAccess,
-            out IntPtr TokenHandle);
-            
-        [DllImport("advapi32.dll", SetLastError=true, CharSet=CharSet.Unicode)]
-        public static extern bool GetTokenInformation(
-            IntPtr TokenHandle,
-            uint TokenInformationClass,
-            IntPtr TokenInformation,
-            uint TokenInformationLength,
-            out uint ReturnLength);
-    }
-    
-    public static class Kernel32
-    {
-        [DllImport("kernel32.dll")]
-        public static extern uint GetLastError();
-        
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr OpenProcess(
-            int dwDesiredAccess,
-            bool bInheritHandle,
-            int dwProcessId);
-    }
+	Add-Type -TypeDefinition @"
+	using System;
+	using System.Diagnostics;
+	using System.Runtime.InteropServices;
+	using System.Security.Principal;
+	
+	[StructLayout(LayoutKind.Sequential)]
+	public struct PROCESS_INFORMATION
+	{
+		public IntPtr hProcess; public IntPtr hThread; public uint dwProcessId; public uint dwThreadId;
+	}
+	
+	[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+	public struct STARTUPINFO
+	{
+		public uint cb; public string lpReserved; public string lpDesktop; public string lpTitle;
+		public uint dwX; public uint dwY; public uint dwXSize; public uint dwYSize; public uint dwXCountChars;
+		public uint dwYCountChars; public uint dwFillAttribute; public uint dwFlags; public short wShowWindow;
+		public short cbReserved2; public IntPtr lpReserved2; public IntPtr hStdInput; public IntPtr hStdOutput;
+		public IntPtr hStdError;
+	}
+	
+	[StructLayout(LayoutKind.Sequential, Size=8)]
+	struct LARGE_INTEGER
+	{
+	public uint LowPart; public int HighPart;
+	}
+	
+	[StructLayout(LayoutKind.Sequential, Size=8)]
+	public struct LUID {
+	public uint LowPart; public int HighPart;
+	}
+	
+	[StructLayout(LayoutKind.Sequential)]
+	public struct TOKEN_STATISTICS
+	{
+		LUID _tokenId; LUID _authenticationId; LARGE_INTEGER _expirationTime; public uint _tokenType;
+		public uint _impersonationLevel; public uint _dynamicCharged; public uint _dynamicAvailable;
+		public uint _groupCount; public uint _privilegeCount; LUID _modifiedId;
+	}
+	
+	public static class Advapi32
+	{
+		[DllImport("advapi32.dll", SetLastError=true, CharSet=CharSet.Unicode)]
+		public static extern bool CreateProcessWithLogonW(
+			String userName,
+			String domain,
+			String password,
+			int logonFlags,
+			String applicationName,
+			String commandLine,
+			int creationFlags,
+			int environment,
+			String currentDirectory,
+			ref  STARTUPINFO startupInfo,
+			out PROCESS_INFORMATION processInformation);
+		
+		[DllImport("advapi32.dll", SetLastError=true, CharSet=CharSet.Unicode)]
+		public static extern bool OpenProcessToken(
+			IntPtr ProcessHandle, 
+			uint DesiredAccess,
+			out IntPtr TokenHandle);
+			
+		[DllImport("advapi32.dll", SetLastError=true, CharSet=CharSet.Unicode)]
+		public static extern bool GetTokenInformation(
+			IntPtr TokenHandle,
+			uint TokenInformationClass,
+			IntPtr TokenInformation,
+			uint TokenInformationLength,
+			out uint ReturnLength);
+	}
+	
+	public static class Kernel32
+	{
+		[DllImport("kernel32.dll")]
+		public static extern uint GetLastError();
+		
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr OpenProcess(
+			int dwDesiredAccess,
+			bool bInheritHandle,
+			int dwProcessId);
+	}
 "@
-    
-    # StartupInfo Struct
-    $StartupInfo = New-Object STARTUPINFO
-    $StartupInfo.dwFlags = 0x00000001
-    $StartupInfo.wShowWindow = 0x0001
-    $StartupInfo.cb = [System.Runtime.InteropServices.Marshal]::SizeOf($StartupInfo)
-    
-    # ProcessInfo Struct
-    $ProcessInfo = New-Object PROCESS_INFORMATION
-    
-    # CreateProcessWithLogonW --> lpCurrentDirectory
-    $GetCurrentPath = (Get-Item -Path ".\" -Verbose).FullName
-    
-    echo "`n[>] Calling Advapi32::CreateProcessWithLogonW"
-    $CallResult = [Advapi32]::CreateProcessWithLogonW($User, $Domain, $Password, $LogonType, $Binary, $Args, 0x04000000, $null, $GetCurrentPath, [ref]$StartupInfo, [ref]$ProcessInfo)
-    
-    if (!$CallResult) {
-        $LastError = [Kernel32]::GetLastError()
-        echo "[!] Mmm, something went wrong! GetLastError returned: $LastError`n"
-    }
-    
-    else {
-        echo "[+]Process Details:"
-        Get-Process -Id $ProcessInfo.dwProcessId |ft
-        
-        # Get proc handle
-        echo "[>] Calling Kernel32::OpenProcess"
-        $ProcHandle = [Kernel32]::OpenProcess(0x0410, $false, $ProcessInfo.dwProcessId)
+	
+	# StartupInfo Struct
+	$StartupInfo = New-Object STARTUPINFO
+	$StartupInfo.dwFlags = 0x00000001
+	$StartupInfo.wShowWindow = 0x0001
+	$StartupInfo.cb = [System.Runtime.InteropServices.Marshal]::SizeOf($StartupInfo)
+	
+	# ProcessInfo Struct
+	$ProcessInfo = New-Object PROCESS_INFORMATION
+	
+	# CreateProcessWithLogonW --> lpCurrentDirectory
+	$GetCurrentPath = (Get-Item -Path ".\" -Verbose).FullName
+	
+	echo "`n[>] Calling Advapi32::CreateProcessWithLogonW"
+	$CallResult = [Advapi32]::CreateProcessWithLogonW($User, $Domain, $Password, $LogonType, $Binary, $Args, 0x04000000, $null, $GetCurrentPath, [ref]$StartupInfo, [ref]$ProcessInfo)
+	
+	if (!$CallResult) {
+		$LastError = [Kernel32]::GetLastError()
+		echo "[!] Mmm, something went wrong! GetLastError returned: $LastError`n"
+	}
+	
+	else {
+		echo "[+]Process Details:"
+		Get-Process -Id $ProcessInfo.dwProcessId |ft
+		
+		# Get proc handle
+		echo "[>] Calling Kernel32::OpenProcess"
+		$ProcHandle = [Kernel32]::OpenProcess(0x0410, $false, $ProcessInfo.dwProcessId)
 		
 		if ($ProcHandle -eq 0) {
 			# This may fail because of permissions? Just checking in case.
@@ -204,6 +204,6 @@ function Invoke-Runas {
 			[system.runtime.interopservices.marshal]::PtrToStructure($NewIntPtr,[type]$TokenStats)
 		}
 		
-    }
+	}
 	
 }
